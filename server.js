9b10ad2b62a754d148a4174541cc25befb1efd23
10Unknownboy <97281187+10Unknownboy@users.noreply.github.com>
2026-09-23 15:38:58 +0530

    gaar

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+let latestPassword = null;
+
+app.post("/password", (req, res) => {
+    latestPassword = req.body.password;
+
+    console.log("Received:", latestPassword);
+
+    res.json({
+        success: true
+    });
+});
+
+app.get("/password", (req, res) => {
+    res.json({
+        password: latestPassword
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
