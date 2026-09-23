8150349c879a67a277340686410d74b563fe12ce
10Unknownboy <97281187+10Unknownboy@users.noreply.github.com>
2026-09-23 15:45:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/password", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
